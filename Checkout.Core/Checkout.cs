b39ccf923ad1708caa37a139d87b9e5423053a89
elliot-echo <elliot.inbox.echo@gmail.com>
2025-11-24 16:43:18 +0000

    feat: Checkout interface and service;

---
+++ Checkout.Core/Checkout.cs
@@ -0,0 +1,11 @@
+﻿using Checkout.Core.Interfaces;
+
+namespace Checkout.Core.Services
+{
+	public class Checkout : ICheckout
+	{
+		public void Scan(string item) { }
+
+		public int GetTotalPrice() => 0;
+	}
+}
---
+++ Checkout.Core/Interfaces/ICheckout.cs
@@ -0,0 +1,15 @@
+﻿namespace Checkout.Core.Interfaces
+{
+	public interface ICheckout
+	{
+		/// <summary>
+		/// Identifies and adds <paramref name="item"/> to the current basket
+		/// </summary>
+		void Scan(string item);
+
+		/// <summary>
+		/// Returns the final price of the items in the basket, with price modifiers applied, if any
+		/// </summary>
+		int GetTotalPrice();
+	}
+}
