0f7508aebf927b81eb07c68c02202ad43a51b51f
elliot-echo <elliot.inbox.echo@gmail.com>
2025-11-24 18:41:07 +0000

    feat: added bundle discount strategy, mock;

---
+++ Checkout.Core/Strategies/BundleDiscountStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout.Core.Strategies
+{
+	public class BundleDiscountStrategy : IPricingStrategy
+	{
+		public int GetTotal(int quantity, int unitPrice) => quantity * unitPrice;
+	}
+}
