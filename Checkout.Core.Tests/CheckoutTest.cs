6d3e7dd30166eed06596da77de764a5c45bc1c6c
elliot-echo <elliot.inbox.echo@gmail.com>
2025-11-24 18:29:02 +0000

    test: scanning the predefined item A with the standard no-discount policy, red;

--- Checkout.Core.Tests/CheckoutTest.cs
+++ Checkout.Core.Tests/CheckoutTest.cs
@@ -1,4 +1,7 @@
-﻿using Checkout.Core.Services;
+﻿using Checkout.Core.Catalogue;
+using Checkout.Core.DTO;
+using Checkout.Core.Services;
+using Checkout.Core.Strategies;
 
 namespace Checkout.Core.Tests
 {
@@ -7,12 +10,15 @@ namespace Checkout.Core.Tests
 		[Fact]
 		public void GetTotal_SingleItem_NoOffers()
 		{
+			var plainAPolicy = new PricingPolicy("A", 50, new StandardPriceStrategy());
+			var catalogue = new PricingCatalogue(plainAPolicy);
+
 			var checkout = new CheckoutService();
-			checkout.Scan("0");
+			checkout.Scan("A");
 
 			var total = checkout.GetTotalPrice();
 
-			Assert.True(total > 0);
+			Assert.True(total == 50);
 		}
 
 		[Fact]
