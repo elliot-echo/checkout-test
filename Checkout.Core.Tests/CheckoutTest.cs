37aa7965de648c0c8e928388ca84353e5602be39
elliot-echo <elliot.inbox.echo@gmail.com>
2025-11-24 17:20:28 +0000

    test: non-zero total from a non-empty basket, red;

---
+++ Checkout.Core.Tests/CheckoutTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Checkout.Core.Tests
+{
+	public class CheckoutTest
+	{
+		[Fact]
+		public void GetTotal_SingleItem_NoOffers()
+		{
+			var checkout = new Checkout();
+			checkout.Scan("0");
+
+			var total = checkout.GetTotalPrice();
+
+			Assert.True(total > 0);
+		}
+	}
+}
