214da5c7dc206238dd787e7fada1b9710e9b94df
elliot-echo <elliot.inbox.echo@gmail.com>
2025-11-24 17:37:46 +0000

    feat: provisional price calculation logic, single item basket test goes green;

---
+++ Checkout.Core/Services/CheckoutService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Checkout.Core.Interfaces;
+
+namespace Checkout.Core.Services
+{
+	public class CheckoutService : ICheckout
+	{
+		private readonly Dictionary<string, int> _basket = new(StringComparer.OrdinalIgnoreCase);
+
+		public void Scan(string item)
+		{
+			if (!_basket.ContainsKey(item))
+			{
+				_basket.Add(item, 1);
+			}
+			else
+			{
+				_basket[item]++;
+			}
+		}
+
+		public int GetTotalPrice() => _basket.Values.Sum();//since the price is an integer, the min price of an item is bound to be 1; then the most naive execution without knowing prices is to return the total quantity
+	}
+}
