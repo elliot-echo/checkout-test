6e276e4db2b4677e77f57855351f9cabd9893b31
elliot-echo <elliot.inbox.echo@gmail.com>
2025-11-24 19:25:34 +0000

    test: consistency check with randomised checkout sequence, green;

--- Checkout.Core.Tests/CheckoutTest.cs
+++ Checkout.Core.Tests/CheckoutTest.cs
@@ -8,6 +8,57 @@ namespace Checkout.Core.Tests
 	public class CheckoutTest
 	{
 		[Fact]
+		public void GetTotal_MultipleDifferentItems_OfferCombination_RandomOrder()
+		{
+			var bundleAPolicy = new PricingPolicy("A", 50, new BundleDiscountStrategy(3, 130));
+			var bundleBPolicy = new PricingPolicy("B", 30, new BundleDiscountStrategy(2, 45));
+
+			var standardPrice = new StandardPriceStrategy();
+			var singleCPolicy = new PricingPolicy("C", 20, standardPrice);
+			var singleDPolicy = new PricingPolicy("D", 15, standardPrice);
+
+			var catalogue = new PricingCatalogue(bundleAPolicy, bundleBPolicy, singleCPolicy, singleDPolicy);
+
+			var checkout = new CheckoutService(catalogue);
+
+			//build the sequence of transactions
+			var transactions = new List<Action>();
+			//three A's for 130
+			transactions.Add(() => checkout.Scan("A"));
+			transactions.Add(() => checkout.Scan("A"));
+			transactions.Add(() => checkout.Scan("A"));
+
+			//two B's for 45 plus one for 30, 75 in total
+			transactions.Add(() => checkout.Scan("B"));
+			transactions.Add(() => checkout.Scan("B"));
+			transactions.Add(() => checkout.Scan("B"));
+
+			//two C's for 20, 40 in total
+			transactions.Add(() => checkout.Scan("C"));
+			transactions.Add(() => checkout.Scan("C"));
+
+			//three D'c for 15, 45 in total
+			transactions.Add(() => checkout.Scan("D"));
+			transactions.Add(() => checkout.Scan("D"));
+			transactions.Add(() => checkout.Scan("D"));
+
+			//randomise it
+			var rng = new Random();
+			for (var i = transactions.Count - 1; i > 0; i--)
+			{
+				var j = rng.Next(0, i + 1);
+				(transactions[i], transactions[j]) = (transactions[j], transactions[i]);
+			}
+
+			//execute in random order to test consistency
+			transactions.ForEach(t => t());
+
+			var total = checkout.GetTotalPrice();
+
+			Assert.Equal(290, total);
+		}
+
+		[Fact]
 		public void GetTotal_SingleItem_NoOffers()
 		{
 			var plainAPolicy = new PricingPolicy("A", 50, new StandardPriceStrategy());
