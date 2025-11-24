26b2655d40f9e4d3894fc356f5cf48594f6ef693
elliot-echo <elliot.inbox.echo@gmail.com>
2025-11-24 18:22:29 +0000

    feat: introduced pricing catalogue for registering and retrieving pricing policies;

---
+++ Checkout.Core/Catalogue/IPricingCatalogue.cs
@@ -0,0 +1,15 @@
+﻿using Checkout.Core.DTO;
+
+namespace Checkout.Core.Catalogue
+{
+	/// <summary>
+	/// Container of <see cref="PricingPolicy"/>
+	/// </summary>
+	public interface IPricingCatalogue
+	{
+		/// <summary>
+		/// Lookup method for obtaining a valid <see cref="PricingPolicy"/> for a product
+		/// </summary>
+		PricingPolicy GetPricingPolicy(string sku);
+	}
+}
---
+++ Checkout.Core/Catalogue/PricingCatalogue.cs
@@ -0,0 +1,20 @@
+﻿using Checkout.Core.DTO;
+using System.Linq;
+
+namespace Checkout.Core.Catalogue
+{
+	public class PricingCatalogue : IPricingCatalogue
+	{
+		private readonly Dictionary<string, PricingPolicy> _policies = new();
+
+		public PricingCatalogue(params PricingPolicy[] policies)
+		{
+			foreach (var policy in policies)
+			{
+				_policies.Add(policy.SKU, policy);
+			}
+		}
+
+		public PricingPolicy GetPricingPolicy(string sku) => _policies[sku];
+	}
+}
